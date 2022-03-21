8b9c57bf2a5f10f2539486d3d47f71751d01d3a4
Nirjas Jakilim <nirjas01@student.sust.edu>
2022-03-21 21:51:30 +0600

    Added logcopier

---
+++ logcopier.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#####################################
+# Author: Nirjas Jakilim
+# Version: v1
+# Description: Copy dpkg.log to a local directory.
+# Usage: ./logcopier.sh
+#####################################
+
+# Change directory to the script location.
+cd $(dirname $0)
+# Create the directory in which we'll store the file.
+if [[ ! -d dpkg ]]; then
+	mkdir dpkg || { echo "Cannot create the directory, stopping script.";
+exit 1; }
+fi
+# Copy the log file to our new directory.
+cp /var/log/dpkg.log dpkg || { echo "Cannot copy dpkg.log to the new
+directory."; exit 1; }
