c30cc198cdb782eadb7130466889059f36b40597
Nirjas Jakilim <nirjas01@student.sust.edu>
2022-03-21 21:54:38 +0600

    Added file.sh

---
+++ find.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#####################################
+# Author: Nirjas Jakilim
+# Version: v1
+# Motive: Find a file.
+# Usage: ./find.sh <file-name>
+#####################################
+
+# Check for the current number of arguments.
+if [[ $# -ne 1 ]]; then
+echo "Wrong number of arguments!"
+echo "Usage: $0 <file-name>"
+exit 1
+fi
+
+# Name of the file to search for.
+file_name=$1
+# Redirect all errors to /dev/null, so they don't clutter the terminal.
+find / -name "${file_name}" 2> /dev/null
